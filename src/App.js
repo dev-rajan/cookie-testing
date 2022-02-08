1e8ffe176b1a43efaa41eaaec1d60d56cf37f888
Rajan Shrestha <rajansoftnep@gmail.com>
2022-02-08 16:03:45 +0545

    build added

--- src/App.js
+++ src/App.js
@@ -1,24 +1,33 @@
-import "./App.css";
+import { useState } from "react";
 import Cookies from "universal-cookie/es6";
 
+import "./App.css";
+
 function App() {
+  const [cookieValue, setCookieValue] = useState("");
+
   const cookies = new Cookies();
 
   const handleAdd = () => {
     cookies.set("abc", "Test Cookie", { path: "/" });
   };
 
+  const handleRead = () => {
+    setCookieValue(cookies.get("Cookie")); // Pacman
+  };
+
   const handleRemove = () => {
     cookies.remove("abc", { path: "/" });
   };
-
-  console.log(cookies.get("Cookie")); // Pacman
 
   return (
     <div className="App">
       <h1>PROGRAMMIZ SESSION COOKIE TEST IN REACT</h1>
       <button onClick={handleAdd}>Add Cookie</button> &nbsp;
+      <button onClick={handleRead}>Read Cookie</button> &nbsp;
       <button onClick={handleRemove}>Remove Cookie</button>
+      <br />
+      <div>Cookie: {cookieValue}</div>
     </div>
   );
 }
